dbb88258d3fa6f98aeb38da043da6d5f3339ee02
Pedro Fauth <fauthpedroa@gmail.com>
2024-03-27 20:59:00 -0300

    class -> Item + construct

--- back/api/Item.php
+++ back/api/Item.php
@@ -1,11 +1,20 @@
 <?php 
 class Item{
     public int $id;
-    public string $nome;
-    public float $valor;
-    public bool $disponivel;
-    public string $categoria;
-    
+    public string $name;
+    public float $value;
+    public bool $available;
+    public string $group;
+    public string $description;
+
+    public function __construct($id, $name, $value, $available, $group, $description){
+        $this->id = $id;
+        $this->name = $name;
+        $this->value = $value;
+        $this->available = $available;
+        $this->group = $group;
+        $this->description = $description;
+    }
 }
 
 
