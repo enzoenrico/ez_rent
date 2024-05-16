c5b01af2e11897355cb51f49f04a1abf0e1861ba
ThiagoKwon <thiago.v.s.kwon@gmail.com>
2024-05-15 21:20:14 -0300

    ajust

--- front/navbar.php
+++ front/navbar.php
@@ -64,7 +64,6 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 <div class='body-size'>
 
   <head>
-    <link rel="stylesheet" href="../front/navbar-style.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
   </head>
 
